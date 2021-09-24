120a4ab6ea4a3c40e599bffac7a802ab33d53439
Davina <davinamr91@gmail.com>
2021-09-24 15:43:50 +0200

    viernes-dav

--- public/js/music.js
+++ public/js/music.js
@@ -48,27 +48,25 @@ listaCD.forEach((object)=>printDiscos(object));
 loadDiscos();
 
 
-//libreria swiper
+//swipper
 
-var swiper = new Swiper(".swiper-container", {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: "auto",
-    coverflowEffect: {
-      rotate: 20,
-      stretch: 0,
-      depth: 200,
-      modifier: 1,
-      slideShadows: true,
-    },
- 
-    pagination: {
-    el: ".swiper-pagination",
-  },
-  loop:true,
-  autoplay: {
-    delay: 500,
-    disableOnInteraction:false,
-  },
+var swiper = new Swiper(".mySwiper", {
+effect: "coverflow",
+grabCursor: true,
+centeredSlides: true,
+slidesPerView: "auto",
+coverflowEffect: {
+rotate: 50,
+stretch: 0,
+depth: 100,
+modifier: 1,
+slideShadows: true,
+},pagination: {
+el: ".swiper-pagination",
+},
+loop:true,
+autoplay: {
+delay: 3000,
+disableOnInteraction:false,
+},
 });
